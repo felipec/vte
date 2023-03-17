89592f17fd32768862b4f2df4ca69e3c3e300c3c
Felipe Contreras <felipe.contreras@gmail.com>
2023-03-17 13:55:52 -0600

    Add timeout hack
    This reimplements 058adf5f (pty: Fix indefinite wait for EOS after
    child-exited, 2019-12-01).

--- src/glib-glue.hh
+++ src/glib-glue.hh
@@ -225,8 +225,8 @@ private:
                         vte::log_exception();
                 }
 
-                /* The Timer may have been re-scheduled from within the callback.
-                 * In this case, the callback must return false!
+                /* The Timer may have been re-scheduled or removed from within
+                 * the callback. In this case, the callback must return false!
                  * m_source_id is now different (since the old source
                  * ID is still associated with the main context until we return from
                  * this function), after which invalidate_source() will be called,
