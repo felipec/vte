749799b1959e01937f1b084245950cf3e9b8f70c
Felipe Contreras <felipe.contreras@gmail.com>
2024-03-12 03:47:51 -0600

    vte: cleanup unused stuff
    In commit 1bcf4bca (pty: Fix exit delay in presence of a grandchild
    process, 2023-08-04) the call to Terminal::queue_child_exited() was
    removed, but not the method.
    
    If we remove that method, a bunch of other methods can be removed as
    well, in addition to m_child_exit_status.
    
    Plus, in commit 7888602c (lib: Rework child exit and EOF handling,
    2019-11-17) the definition of the method Terminal::pty_channel_eof() was
    removed, but not the declaration.
    
    Let's remove all this unused stuff.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- src/vteinternal.hh
+++ src/vteinternal.hh
@@ -291,7 +291,6 @@ public:
         guint m_pty_output_source{0};
         bool m_pty_input_active{false};
         pid_t m_pty_pid{-1};           /* pid of child process */
-        int m_child_exit_status{-1};   /* pid's exit status, or -1 */
         bool m_eos_pending{false};
         VteReaper *m_reaper;
 
@@ -971,7 +970,6 @@ public:
         void pty_termios_changed();
         void pty_scroll_lock_changed(bool locked);
 
-        void pty_channel_eof();
         bool pty_io_read(int const fd,
                          GIOCondition const condition,
                          int amount = -1);
@@ -984,7 +982,6 @@ public:
         bool terminate_child () noexcept;
         void child_watch_done(pid_t pid,
                               int status);
-        void emit_child_exited();
 
         void im_commit(std::string_view const& str);
         void im_preedit_set_active(bool active) noexcept;
@@ -1124,7 +1121,6 @@ public:
 
         void queue_cursor_moved();
         void queue_contents_changed();
-        void queue_child_exited();
         void queue_eof();
 
 #ifdef WITH_A11Y
